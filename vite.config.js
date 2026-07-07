c45cbbc98b838fa33d529dd6291a28c6025de2b9
mahdiekhalili2015-prog <mahdie.khalili.2015@gmail.com>
2026-07-07 06:29:50 +0300

    Fix GitHub Pages configuration

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
+  base: "/cesium-assignment/",
+
   define: {
-    CESIUM_BASE_URL: JSON.stringify('/cesium')
+    CESIUM_BASE_URL: JSON.stringify("/cesium-assignment/cesium"),
   },
+
   plugins: [
     viteStaticCopy({
       targets: [
         {
-          src: 'node_modules/cesium/Build/Cesium/*',
-          dest: 'cesium'
-        }
-      ]
-    })
-  ]
-})
+          src: "node_modules/cesium/Build/Cesium/*",
+          dest: "cesium",
+        },
+      ],
+    }),
+  ],
+});
